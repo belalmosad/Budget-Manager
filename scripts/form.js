60ddf24ee4f206622108bfdd429dcb9dd993487b
unknown <belalelmeligy516@gmail.com>
2022-04-26 05:39:13 +0200

    removed extra spaces and lines fot better code readability

--- scripts/form.js
+++ scripts/form.js
@@ -6,13 +6,10 @@ document.forms[0].onsubmit= function(e){
     var category = document.getElementById('category').value;
     var description = document.getElementById('description').value;
     var cost = document.getElementById('cost').value;
-
     dataArr.push(category);
     dataArr.push(description);
     dataArr.push(cost);
-
     var allData = JSON.parse(localStorage.getItem("data"));
     allData.push(dataArr);
-
     localStorage.setItem("data", JSON.stringify(allData))
 }
